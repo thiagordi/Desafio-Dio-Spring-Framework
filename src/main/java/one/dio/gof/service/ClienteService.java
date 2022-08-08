015f99085dae627a4cf4acce1b1d923a6c155c68
thiago <thiago-r-d@hotmail.com>
2022-08-08 07:04:29 -0300

    primeiro commit

---
+++ src/main/java/one/dio/gof/service/ClienteService.java
@@ -0,0 +1,26 @@
+package one.dio.gof.service;
+
+import one.dio.gof.model.Cliente;
+
+public class ClienteService {
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    public Cliente buscarPorId() {
+
+
+        return null;
+    }
+
+    public void inserir(Cliente cliente) {
+    }
+
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    public void deletar(Long id) {
+
+    }
+}
